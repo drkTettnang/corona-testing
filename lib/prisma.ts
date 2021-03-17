17db69fe68026fdad81cbda25ca80de25d599d05
Klaus Herberth <klaus.herberth@drk-tettnang.de>
2021-03-17 09:40:35 +0100

    chore: reuse prisma client in dev mode

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,7 +1,17 @@
 import { PrismaClient } from "@prisma/client";
 import dayjs from "dayjs";
 
-const prisma = new PrismaClient();
+let prisma: PrismaClient
+
+if (process.env.NODE_ENV === 'production') {
+    prisma = new PrismaClient()
+} else {
+    if (!(global as any).prisma) {
+        (global as any).prisma = new PrismaClient()
+    }
+
+    prisma = (global as any).prisma
+}
 
 export default prisma;
 
