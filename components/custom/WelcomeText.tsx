91e3d4234d35a3fd2839cb0392097d351512f2f8
Klaus Herberth <klaus.herberth@drk-tettnang.de>
2021-03-19 22:38:23 +0100

    chore: update welcome text

--- components/custom/WelcomeText.tsx
+++ components/custom/WelcomeText.tsx
@@ -5,13 +5,20 @@ import Config from '../../lib/Config';
 export default function WelcomeText() {
     return (
         <>
-            <Typography variant="h3" gutterBottom={true}>Corona-Schnellteststation Meckenbeuren</Typography>
+            <Typography variant="h3" gutterBottom={true}>Kommunales Corona-Schnelltestzentrum Meckenbeuren</Typography>
 
             <Typography variant="body1" paragraph={true}>
-                Im Rahmen der Landtagswahlen am 14. März haben Wahlhelfer und von der Gemeinde Meckenbeuren
-                berechtigte Personen die Möglichkeit sich kostenlos auf das Sars-CoV-2 testen zu lassen.
-                Die Teststation wird von ehrenamtlichen Helfern des DRK Ortsvereins Tettnang e.V.
-                im "Kultur am Gleis 1", Bahnhofplatz 1, 88074 Meckenbeuren, betrieben.
+                Corona-Tests können einen wichtigen Beitrag für die frühzeitige Erkennung einer Corona-Infektion leisten.
+                Die Gemeinde möchte zur Eindämmung der Corona-Pandemie einen weiteren Beitrag leisten, indem sie ein
+                Schnelltestzentrum gemeinsam mit dem DRK Ortsverein Tettnang e.V. betreibt.
+            </Typography>
+            <Typography variant="body1" paragraph={true}>
+                Ab Dienstag, den 23.03.2021 haben deshalb <strong>alle Bürgerinnen und Bürger</strong> die Möglichkeit,
+                sich dienstags und donnerstags, jeweils 17 - 20 Uhr, kostenlos im
+                <strong>kommunalen Schnelltestzentrum</strong> in Kehlen testen zu lassen.
+            </Typography>
+            <Typography variant="body1" paragraph={true}>
+                Adresse: <strong>Dorfgemeinschaftshaus Kehlen, Hügelstraße 11, 88074 Meckenbeuren</strong>.
             </Typography>
             <Typography variant="body1" paragraph={true}>
                 Weitere Informationen erhalten Sie auf unserer <Link href={Config.HOMEPAGE}>Corona Übersichtsseite</Link>.
