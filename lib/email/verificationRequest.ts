a2952c8b935138ea88e0d96b074556978b2fe6fe
Klaus Herberth <klaus.herberth@drk-tettnang.de>
2021-03-28 15:14:05 +0200

    feat: test if email domain is valid

--- lib/email/verificationRequest.ts
+++ lib/email/verificationRequest.ts
@@ -3,6 +3,7 @@ import { VerificationRequestTemplate } from "../templates";
 import { render } from 'mjml-react';
 import { htmlToText } from 'html-to-text';
 import VerificationRequest from "../../templates/email/VerificationRequest";
+import { resolveMx } from "dns";
 
 function generateBody(data: { url: string, site: string, email: string }, verificationRequestTemplate: VerificationRequestTemplate) {
     const HTML = render(verificationRequestTemplate(data), {
@@ -27,19 +28,31 @@ function generateBody(data: { url: string, site: string, email: string }, verifi
     };
 }
 
-export const sendVerificationRequest = ({ identifier: email, url, token, baseUrl, provider }) => {
+function hasValidDomain(email: string): Promise<boolean> {
+    const parts = email.split('@');
+
+    if (parts.length < 2 || !parts[parts.length - 1]) {
+        return Promise.resolve(false);
+    }
+
+    return new Promise(resolve => {
+        resolveMx(parts[parts.length - 1], (err, addresses) => {
+            resolve(!err && addresses && addresses.length > 0);
+        });
+    });
+}
+
+export const sendVerificationRequest = async ({ identifier: email, url, token, baseUrl, provider }) => {
+    if (!email || /[,;]/.test(email) || email.indexOf('@') < 1 || !await hasValidDomain(email)) {
+        console.log(`Mail address ("${email}") not valid`);
+
+        return Promise.reject(new Error('SEND_VERIFICATION_EMAIL_ERROR'));
+    }
+
     return new Promise<void>((resolve, reject) => {
         const { from } = provider
         // Strip protocol from URL and use domain as site name
         const site = baseUrl.replace(/^https?:\/\//, '');
-
-        if (!email || /[,;]/.test(email) || email.indexOf('@') < 1) {
-            console.log(`Mail address ("${email}") not valid`);
-
-            reject(new Error('SEND_VERIFICATION_EMAIL_ERROR'));
-
-            return;
-        }
 
         const body = generateBody({ url, site, email }, VerificationRequest);
 
