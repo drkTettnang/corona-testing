0fe6ce7a83e85d8ee018f15a771ab8ec76b44352
Klaus Herberth <klaus.herberth@drk-tettnang.de>
2021-04-25 11:12:15 +0200

    chore: upgrade next-auth

--- auth.d.ts
+++ auth.d.ts
@@ -1,8 +1,16 @@
-import { Role } from "@prisma/client";
+import { Role, User as PrismaUser } from "@prisma/client";
 import "next-auth";
 
 declare module "next-auth" {
-  interface User {
-    role: Role
+  interface Session {
+    user?: {
+      name?: string | null
+      email?: string | null
+      image?: string | null
+      role?: Role | null
+    }
+  }
+
+  interface User extends PrismaUser {
   }
 }
