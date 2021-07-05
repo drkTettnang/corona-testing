6aeadde2d332914462ffe8d27e90636ed62f8d1f
Klaus Herberth <klaus.herberth@drk-tettnang.de>
2021-07-05 14:01:41 +0200

    chore: update test log template

--- templates-example/TestLogBody.tsx
+++ templates-example/TestLogBody.tsx
@@ -25,19 +25,18 @@ const TestLogBody: React.FC<Props> = ({ cwa }) => {
             <ul>
                 <li>Mir ist bewusst, dass sich durch die Testmethode leider keine absolute Sicherheit garantieren
                 lässt und die Analysemethode unrichtige oder ungültige Testergebnisse hervorbringen kann. Ich werde daraus
-                keinerlei Ansprüche gegenüber dem DRK ableiten.</li>
+                keinerlei Ansprüche gegenüber dem Betreiber der Teststelle ableiten.</li>
 
                 <li>Mir ist bewusst, dass es bei oder nach dem Abstrich, zu Irritationen, Reizungen und in seltenen
                 Fällen zu Blutungen an der Abstrichstelle kommen kann.</li>
 
-                <li>Mir ist bewusst, dass ein positives Testergebnis in Bezug auf das Vorhandensein von Sars-CoV-2
-                Viren bedeutet, dass ich unverzüglich häusliche Quarantäne einhalten muss. Das gilt ebenfalls
-                für Personen, die mit mir in einem Haushalt leben. (Siehe CoronaVO Absonderung)</li>
+                <li>Mir ist bewusst, dass ein positives Testergebnis bedeutet, dass ich mich unverzüglich in
+                Absonderung begeben muss. Das gilt ebenfalls für Personen, die mit mir in einem Haushalt leben.
+                (Siehe CoronaVO Absonderung)</li>
 
                 <li>Ich weiß, dass ein positives Schnelltestergebnis durch einen anschließenden PCR-Test bestätigt
                 werden sollte. Ich werde unverzüglich mit der zuständigen Corona-Anlaufstelle Kontakt
-                aufnehmen, um einen Testtermin zu vereinbaren. Erst wenn das Ergebnis der PCR-Testung
-                negativ ausfällt, kann die Quarantäne aufgehoben werden.</li>
+                aufnehmen, um einen Testtermin zu vereinbaren.</li>
 
                 <li>Ich weiß, dass die Teilnahme an der Testungen freiwillig und kostenlos ist.</li>
 
@@ -46,8 +45,8 @@ const TestLogBody: React.FC<Props> = ({ cwa }) => {
 
             <Typography variant="body1">
                 Mit meiner Unterschrift erkläre ich mich mit der Durchführung des PoC-Antigen-Tests durch
-                medizinisches Fachpersonal oder geschulte DRK-Sanitätshelfer einverstanden und willige in die umseitig
-                erklärte Datenverarbeitung ein.
+                geschultes Personal, sowie dem Versand des Ergebnisses an die bei der Anmeldung genutzten E-Mail Adresse
+                einverstanden. Die umseitig abgedruckten Hinweise zur Datenverarbeitung habe ich zur Kenntnis genommen.
             </Typography>
         </>
     )
